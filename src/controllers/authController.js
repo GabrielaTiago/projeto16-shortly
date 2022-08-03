674fac07d89cc904fcd04fa1c3e51890f5e9dea6
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-08-02 23:56:49 -0300

    Chore: Created the signIn controller with jwt authentication

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,39 @@
 import { connection } from "../database/postgres.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
+async function signInUsers(req, res) {
+  const { email, password } = req.body;
+
+  try {
+    const { rows: user, rowCount } = await connection.query(
+      "SELECT * FROM users WHERE email = $1",
+      [email]
+    );
+    
+    if (rowCount === 0) return res.sendStatus(404);
+    
+    const validEmail = user[0].email;
+    const validPassword = bcrypt.compareSync(password, user[0].password);
+
+    if (validEmail && validPassword) {
+      const token = jwt.sign({ email, password }, process.env.JWT_KEY, { expiresIn: "1d" });
+
+      res.status(200).send({
+        message: "Authentication Success",
+        token: token
+      });
+
+    } else {
+        res.status(401).send("Authentication Failure");
+    }
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+}
 
 async function signUpUsers(req, res) {
   const { name, email, password } = req.body;
@@ -22,11 +55,10 @@ async function signUpUsers(req, res) {
     );
 
     res.sendStatus(201);
-
   } catch (error) {
     console.error(error);
     res.sendStatus(500);
   }
 }
 
-export { signUpUsers };
+export { signInUsers, signUpUsers };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { signUpUsers } from "../controllers/authController.js";
+import { signInUsers, signUpUsers } from "../controllers/authController.js";
 import { signInMiddleware } from "../middlewares/signInMiddleware.js";
 import { signUpMiddleware } from "../middlewares/signUpMiddlewares.js";
 
 const authRoutes = Router();
 
 authRoutes.post("/signup", signUpMiddleware, signUpUsers);
-authRoutes.post("/signin", signInMiddleware);
+authRoutes.post("/signin", signInMiddleware, signInUsers);
 
 export { authRoutes };
